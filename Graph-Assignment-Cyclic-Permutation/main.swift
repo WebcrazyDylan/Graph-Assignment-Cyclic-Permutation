47745a40c99bea2deec4918aa3d1c9903f4bae65
WebcrazyDylan <webcrazy@naver.com>
2021-08-31 17:20:24 -0700

    Update Code

--- Graph-Assignment-Cyclic-Permutation/main.swift
+++ Graph-Assignment-Cyclic-Permutation/main.swift
@@ -7,29 +7,58 @@
 
 import Foundation
 
-func dfs(start: Int, nodes: inout [Int], visited: inout [Bool]) {
-  if visited[start] {
-    return
-  }
-  visited[start] = true
-  dfs(start: nodes[start], nodes: &nodes, visited: &visited)
-}
+// Cyclic Permutation
+func cyclicPermutation() {
+    print("Please input for Cyclic Permutation")
+    let preNumberOfCases = readLine()!.split(separator: " ").map { Int($0) }
+    let numberOfCases = preNumberOfCases[0]!
 
-var tests = Int(readLine()!)!
-while tests > 0 {
-  let n = Int(readLine()!)!
-  var nodes = readLine()!.split(separator: " ").map { Int($0)! }
-  nodes.insert(0, at: 0)
-
-  var visited = [Bool](repeating: false, count: n + 1)
-  var count = 0
-  for v in 1...n {
-    if !visited[v] {
-      dfs(start: v, nodes: &nodes, visited: &visited)
-      count += 1
+    
+    for _ in 0..<numberOfCases {
+        let preN = readLine()!.split(separator: " ").map { Int($0) }
+        let N = preN[0]!
+        let permutation = readLine()!.split(separator: " ").map { Int($0) }
+        var alreadyPassed: Set = [1]
+        var numOfCycles = 0
+        for i in 1...N {
+            // if the integer is already passed
+            if alreadyPassed.contains(i) {
+                continue
+            }
+            // if the integer has a roop
+            if permutation[i - 1] == i {
+                numOfCycles += 1
+                continue
+            }
+            // search for a cycle
+            var foundCycle: Bool = false
+            var next = permutation[i - 1]!
+            while (foundCycle == false) {
+                alreadyPassed.insert(next)
+                next = permutation[next - 1]!
+                if alreadyPassed.contains(next) {
+                    numOfCycles += 1
+                    foundCycle = true
+                }
+            }
+        }
+        
+        print(numOfCycles)
     }
-  }
-  print(count)
-  tests -= 1
 }
 
+
+cyclicPermutation()
+
+/*
+ Sample Input 1
+ 2
+ 8
+ 3 2 7 8 1 4 5 6
+ 10
+ 2 1 3 4 5 6 7 9 10 8
+ 
+ Sample Output 1
+3
+7
+ */
